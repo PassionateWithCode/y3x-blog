87cde8a8b851da6f664d18e76027f836108523da
wumao <webwwy@gmail.com>
2021-12-29 15:37:30 +0800

    style: change types

--- src/utils/http/axios.ts
+++ src/utils/http/axios.ts
@@ -1,29 +1,36 @@
-import axios from 'axios'
-import type { AxiosResponse, AxiosError } from 'axios'
-import { axiosConfig, ContentTypeEnum } from './config'
+/*
+ * @Author: wumao
+ * @Date: 2021-11-05 17:04:56
+ * @LastEditors: wumao
+ * @LastEditTime: 2021-12-29 15:19:02
+ * @Description: file content
+ */
+import axios from "axios";
+import type { AxiosResponse, AxiosError } from "axios";
+import { axiosConfig, ContentTypeEnum } from "./config";
 
 const api = axios.create({
   baseURL: axiosConfig.BASE_URL,
   timeout: 10 * 1000,
-  headers: { 'Content-Type': ContentTypeEnum.JSON },
-})
+  headers: { "Content-Type": ContentTypeEnum.JSON },
+});
 
-api.interceptors.response.use(successFn, errorFm)
+api.interceptors.response.use(successFn, errorFm);
 
 function successFn(response: AxiosResponse): AxiosResponse {
-  return response
+  return response.data;
 }
 function errorFm(error: AxiosError): Promise<Record<string, unknown>> {
-  const res = error.response
+  const res = error.response;
   if (res !== undefined) {
     switch (res.status) {
       // todo 封装统一的提示
       case 400:
         console.log("xxx");
-        break
+        break;
     }
   }
-  return Promise.reject(error)
+  return Promise.reject(error);
 }
 
-export default api
+export default api;
