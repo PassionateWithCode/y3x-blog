2223a12b7f0abcb5f6dd25221dcfc7c73cf762c4
wumao <webwwy@gmail.com>
2022-02-21 13:31:49 +0800

    feat: add global types

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,6 +1,12 @@
-type Recordable<T = any> = Record<string, T>;
+type Recordable<T = any> = Record<string, T>
 
-type UnknownObj = Recordable<unknown>;
+type UnknownObj = Recordable<unknown>
 
 // todo 报错的原因是因为cypress这个库里面也有这个,待解决
 // type Nullable<T> = T | null
+
+// 获取Promise<value>里面的value的值
+type GetPromiseRes<T extends Promise<any>> = T extends Promise<infer Type> ? Type : never
+
+// 获取数组的第一项
+type GetFirst<T extends Array<any>> = T extends [infer Type, ...Array<unknown>] ? Type : never
